6b940b1c1fa4e30ef506360875931edf169141fc
noe stauffert <noe.stauffert@gmail.com>
2016-09-13 22:08:15 -0700

    updated config and db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount RedactorRails::Engine => '/redactor_rails'
+  devise_for :admins, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  root to: 'projects#index'
+  get 'work/:slug', to: 'projects#show', as: :project
+
+  get 'about', to: 'pages#about'
+  get 'resume', to: 'pages#resume'
+  get 'pdf', to: 'pages#pdf'
+
+  get 'blog', to: 'posts#index'
+  get 'blog/:slug', to: 'posts#show', as: :post
 end
