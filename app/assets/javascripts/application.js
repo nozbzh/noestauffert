af3fd1a8ab46cc96034e9ce6b854a9eb65f78b96
noe stauffert <noe.stauffert@gmail.com>
2016-09-19 21:52:03 -0700

    moved project JS to application.js, trying to fix activeadmin production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,3 +11,26 @@ $(document).ready(function() {
   $(".redact img").addClass("img-responsive center-block");
   $(".redact a").attr('target', '_blank');
 });
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#img_prev')
+        .attr('src', e.target.result)
+        .width(450)
+        .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+
+$(function(){
+  $('body').on('change', '#project_image', function(){
+    readURL(this);
+    $('#prev-panel').css({ display: "block" });
+  });
+});
